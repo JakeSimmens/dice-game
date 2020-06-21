c764df7fd8ed19cce22b3e253163adb6714efa94
Jake Simmens <rocknjake@gmail.com>
2020-06-20 21:08:01 -0500

    Create Battle class to track players.  Create html to run code. Place my command prompt in its own js file.

--- app.js
+++ app.js
@@ -1,58 +1,95 @@
 
-//Gives user ability to interact with their program on the command line.
-//Pass in a call back function that will run based on the commands entered.
-//Typing 'exit' will terminate the function
-function myCommandLine(callback) {
+//Requires class Players as arguments for constructor
+class Battle {
+    //compare rolls
+    //choose how many die to roll
+    constructor(...contenders) {
+        //list of players
+        this.players = [];
+        this.lastRoll = [];
+        for (let contender of contenders) {
+            this.players.push(contender);
+        };
+    }
 
-    console.log("= = = = MY COMMAND LINE = = = =");
-    process.stdin.setEncoding("utf8");  //set to read characters
-    process.stdin.resume();  //turn on reading user input
+    get numPlayers() {
+        return this.players.length;
+    }
 
-    //prompt for input...another way to do console.log()
-    process.stdout.write("\n>>> ");
+    get whosTurn() {
+        return this.players[0];
+    }
 
-    //reads command line until exit is typed
-    process.stdin.on("data", function (data) {
+    nextPlayer() {
+        let turnOver = this.players.shift();
+        this.players.push(turnOver);
+        return this.players[0];
+    }
 
-        const cmdString = data.trim();
+    //Returns an array of the requested number of dice rolled defaulted to 6 sides.
+    roll(numDice, numSides = 6) {
 
-        if (cmdString === "exit") {
+        const diceArray = [];
+        let result;
 
-            process.stdin.pause();  //pause reading user input
-            console.log("=======END=======");
-            return;
+        if (numDice < 1 || numSides < 2) {
+            console.log("rollDice requires at least 1 die and 2 sides");
+            return [];
         }
 
-        //console.log(`length: ${cmdString.length}`);
-        callback(cmdString);
-        process.stdout.write("\n>>> ");
-    });
+        for (let i = 0; i < numDice; i++) {
+            result = Math.floor(Math.random() * numSides + 1);
+            diceArray.push(result);
+        }
+
+        this.lastRoll = diceArray;
+
+        return this.lastRoll;
+
+    }
+
+    largeToSmall() {
+        this.lastRoll.sort((a, b) => {
+            return b - a;
+        });
+    }
+
+    smallToLarge() {
+        this.lastRoll.sort((a, b) => {
+            return a - b;
+        });
+    }
 
 }
 
-//Returns an array of the requested number of dice rolled defaulted to 6 sides.
-function rollDice(numDice, numSides = 6) {
-
-    const diceArray = [];
-    let result;
-
-    if (numDice < 1 || numSides < 2) {
-        console.log("rollDice requires at least 1 die and 2 sides");
-        return [];
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.lastRoll = [];
+        this.wins = 0;
+        this.losses = 0;
     }
 
-    for (let i = 0; i < numDice; i++) {
-        result = Math.floor(Math.random() * numSides + 1);
-        diceArray.push(result);
+    get winPercent() {
+        if (!(this.wins + this.losses)) {
+            return 0;
+        }
+        return this.wins / (this.wins + this.losses);
     }
 
-    return diceArray;
+    recordWin() {
+        this.wins++;
+    }
 
+    recordLoss() {
+        this.losses++;
+    }
 }
 
-myCommandLine((userInput) => {
-    if (userInput === "roll") {
-        console.log(rollDice(5));
-    }
-});
+//test data
+const jake = new Player("Jake");
+const elise = new Player("Elise");
+const isaac = new Player("Isaac");
+const elijah = new Player("Elijah");
+const risk = new Battle(jake, elise, isaac, elijah);
 
