92a1e87b1bed37f3ce387ce24bbfbd8a4c1d053b
Jake Simmens <rocknjake@gmail.com>
2020-07-22 22:05:10 -0500

    Add RiskGame and PageElements classes.  Simplify Player class.

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
-const attackerResults = document.querySelector("#attackerResults");
-const defenderResults = document.querySelector("#defenderResults");
-const compareResults = document.querySelector("#compareResults");
+const elemAttackerResults = document.querySelector("#attackerResults");
+const elemDefenderResults = document.querySelector("#defenderResults");
+const elemCompareResults = document.querySelector("#compareResults");
 const btnAttackerRoll = document.querySelector("#attackerRoll");
 const btnDefenderRoll = document.querySelector("#defenderRoll");
-const attackerNumDice = document.querySelector("#attackerNumDice");
-const defenderNumDice = document.querySelector("#defenderNumDice");
-const attackLives = document.querySelector("#attackerLives");
-const defendLives = document.querySelector("#defenderLives");
+const elemNumOfAttackerDice = document.querySelector("#attackerNumDice");
+const elemNumOfDefenderDice = document.querySelector("#defenderNumDice");
+const elemAttackerLives = document.querySelector("#attackerLives");
+const elemDefenderLives = document.querySelector("#defenderLives");
+
+class RiskGame {
+    constructor(...player) {
+        this.player = player;
+        this.numberOfPlayers = player.length;
+        this.NUM_OF_DIE_SIDES = 6;
+        this.currentPlayersTurn = 0;
+        this.troopLocationsByPlayer = [];
+        for (let i = 0; i < this.numberOfPlayers; i++) {
+            this.troopLocationsByPlayer.push({});
+        }
+    }
+
+    setupGameBoard() {
+        if (this.troopLocationsByPlayer.length > 0) {
+            console.log("game board already setup");
+            return;
+        }
+        this._assignTroopsToLocations();
+        this._assignStartingPlayer();
+    }
+
+    _assignTroopsToLocations() {
+        this.troopLocationsByPlayer[0].usa = 20;
+        this.troopLocationsByPlayer[1].mexico = 20;
+    }
+
+    _assignStartingPlayer() {
+        this.currentPlayersTurn = 0;
+    }
+}
+
+class PageElementsForRiskGame {
+    constructor() {
+        this.attackerResults = document.querySelector("#attackerResults");
+        this.defenderResults = document.querySelector("#defenderResults");
+        this.compareResults = document.querySelector("#compareResults");
+        this.btnAttackerRoll = document.querySelector("#attackerRoll");
+        this.btnDefenderRoll = document.querySelector("#defenderRoll");
+        this.numOfAttackerDice = document.querySelector("#attackerNumDice");
+        this.numOfDefenderDice = document.querySelector("#defenderNumDice");
+        this.attackerLives = document.querySelector("#attackerLives");
+        this.defenderLives = document.querySelector("#defenderLives");
+    }
+}
 
 
-
-
-
-//Requires class Players as arguments for constructor
-//input
-//Attacker object:  Player class, number of lives
-//Defender object:  Player class, number of lives
-//# of sides on die
-//Button for attacker roll
-//Button for defender roll
-//Input for attacker number of dice to roll
-//Input for defender number of dice to roll
-class DiceMatchFor2Players {
+class DiceMatchAttackerVsDefender {
 
     constructor(
         { attacker, attackerLives: attackerRemainingLives } = {},
@@ -101,7 +134,7 @@ class DiceMatchFor2Players {
         let tallyOfLosses = this.compareDiceArrays(attackerRollSorted, defenderRollSorted);
 
         this.displayBattleResults(attackerRollSorted, defenderRollSorted, tallyOfLosses,
-            attackerResults, defenderResults, compareResults);
+            elemAttackerResults, elemDefenderResults, elemCompareResults);
 
         return {
             attackerLosses: tallyOfLosses.attacker,
@@ -218,6 +251,6 @@ const elise = new Player("Elise");
 //const isaac = new Player("Isaac");
 //const elijah = new Player("Elijah");
 //const risk = new DiceMatch({ attacker: jake, attackerLives: 10 }, { defender: elise, defenderLives: 7 }, DIE_SIDES);
-const risk = new DiceMatchFor2Players({ attacker: jake, attackerLives: 10 }, { defender: elise, defenderLives: 10 }, DIE_SIDES, btnAttackerRoll, btnDefenderRoll, attackerNumDice, defenderNumDice, attackLives, defendLives);
+const risk = new DiceMatchAttackerVsDefender({ attacker: jake, attackerLives: 10 }, { defender: elise, defenderLives: 10 }, DIE_SIDES, btnAttackerRoll, btnDefenderRoll, elemNumOfAttackerDice, elemNumOfDefenderDice, elemAttackerLives, elemDefenderLives);
 
 
--- player.js
+++ player.js
@@ -1,23 +1,9 @@
 class Player {
     constructor(name) {
         this.name = name;
-        this.lastRoll = [];
+        this.rollHistory = []; //array of arrays
         this.wins = 0;
         this.losses = 0;
     }
 
-    get winningPercentage() {
-        if (!(this.wins + this.losses)) {
-            return 0;
-        }
-        return this.wins / (this.wins + this.losses);
-    }
-
-    recordWin() {
-        this.wins++;
-    }
-
-    recordLoss() {
-        this.losses++;
-    }
 }
